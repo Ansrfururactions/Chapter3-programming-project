42fdbe88d9fbc418ae48837d95709532ec8b9ebe
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-10-01 19:10:42 -0400

    MEEPMEEP

--- Chapter3 programming project.cpp
+++ Chapter3 programming project.cpp
@@ -1,18 +1,20 @@
 
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-	int cookieamt;//defines the integer for the user to input later
-	float cupsugar =cookieamt* (1.5 / 48), cupbutter =cookieamt * (1 / 48), cupflour = cookieamt * (2.75 / 48);//defines the amount for each ingredient based on the numbers given
-
+	float cookieamt;
+	float cupsugar =(1.5 / 48), cupbutter =(1.0 / 48), cupflour =(2.75 / 48); //defines the amount for each ingredient based on the numbers given
 
 
 	cout << "please enter your desired amount of cookies";
-	cin >> cookieamt;//allows the u
+	cin >> cookieamt;//allows the user to input their desired amount, which gets assign to the cookieamt integer
 
-	cout << "in order to make " << cookieamt << " cookies, you need " << cupsugar << "cups of sugar, " << cupbutter << "cups of butter, and " << cupflour << "cups of flour" << endl;
+	cupsugar = cookieamt * cupsugar, cupbutter = cookieamt * cupbutter, cupflour = cookieamt * cupflour;
+	
+	cout << "in order to make " << setprecision(2) << cookieamt << " cookies, you need " << setprecision (2) << cupsugar << "cups of sugar, " << setprecision (2) << cupbutter << "cups of butter, and " << setprecision (3) << cupflour << "cups of flour" << endl;
 
 }
