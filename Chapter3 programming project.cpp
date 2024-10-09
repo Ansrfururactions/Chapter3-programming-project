27dec375aae5a6f892f991d3037e367d3e47e780
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-10-09 15:14:00 -0400

    needs to be random

--- Chapter3 programming project.cpp
+++ Chapter3 programming project.cpp
@@ -7,11 +7,25 @@ using namespace std;
 
 int main()
 {
+   
     default_random_engine generator;
-    uniform_int_distribution<int> distribution(-999, 999);
+    uniform_int_distribution<int> distribution(1, 900);
+ 
+    int variableone = distribution(generator);
+    int variabletwo = distribution(generator);
 
-    int variableone = distribution(generator), variabletwo = distribution(generator), answer=variableone + variabletwo;
+    int answer= variableone + variabletwo, studentresponse; //defines all variables relevant to answering the question
+    
+    cout << variableone << " + " << variabletwo << " = ? " << endl;
+    cin >> studentresponse;
 
-    cout << 
+    if (answer == studentresponse)
+    
+        cout << "correct!" << endl;
+    else
+        cout << "incorrect, the answer is " << answer << endl;
+
+
+    
 }
 
